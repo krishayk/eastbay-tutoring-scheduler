00486eb67e265dece16e91e4d32c09122d0af07f
krishayk <krishay.k.30@gmail.com>
2025-06-15 17:28:49 -0700

    Revert: Only show time range on client booking form, not on tutor weekly schedule

--- src/components/WeeklySchedule.jsx
+++ src/components/WeeklySchedule.jsx
@@ -315,7 +315,7 @@ export default function WeeklySchedule() {
             <tbody>
               {TIME_SLOTS.map(time => (
                 <tr key={time}>
-                  <td className="p-4 border-b text-center font-medium bg-gray-50 sticky left-0 z-10">{time} - {getEndTime(time)}</td>
+                  <td className="p-4 border-b text-center font-medium bg-gray-50 sticky left-0 z-10">{time}</td>
                   {DAYS.map(day => {
                     const slotState = getSlotState(day, time);
                     const isUnavailable = slotState === 'unavailable';
@@ -356,7 +356,7 @@ export default function WeeklySchedule() {
                         {/* Floating tooltip */}
                         {hoveredSlot === `${day}-${time}` && (
                           <div className="absolute left-1/2 -translate-x-1/2 top-full mt-2 bg-white border border-gray-300 rounded shadow-lg px-3 py-2 text-xs text-gray-800 z-20 whitespace-nowrap animate-fade-in">
-                            {day}, {time} - {getEndTime(time)} <br />
+                            {day}, {time} <br />
                             {isRecurring 
                               ? 'Recurring Unavailable'
                               : isUnavailable 
